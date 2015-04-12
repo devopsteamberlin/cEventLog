a47a54f0682f19c73311ef4fa2754f6524d2baad
Jacob Benson <jacob.benson@gmail.com>
2015-04-12 06:35:03 -0500

    Initial Upload
    Initial Upload to PowerShell.org repo

---
+++ DSCResources/Examples/cEventLogConfig.ps1
@@ -0,0 +1,22 @@
+﻿Configuration cTestEventLog{
+
+    Import-DSCResource -Name cEventLog
+
+    node $YourNode{
+
+        cEventLog Test{
+
+            LogName = "Application"
+            LogPath = "E:\EventViewer"
+            LogMode = "Circular"
+            MaximumSizeinBytes = 4096MB
+
+        }
+    }
+}
+
+cTestEventLog -OutputPath $YourPath
+
+Copy-Item -Path $YourModuleFolder -Destination $DestinationModuleFolder -Recurse -Force -Verbose
+
+Start-DSCConfiguration -Wait -Force -Verbose -Path $YourPath
---
+++ DSCResources/Files/cEventLOg.ps1
@@ -0,0 +1,32 @@
+﻿#Name the Resource
+#$ResourceName = '<ResourceName'
+$ResourceName = 'cEventLog'
+
+#Create Module Path
+$ResourcePath = <SomePath>
+
+Import-Module -Name xDSCResourceDesigner
+
+#Create the Resource and define Resource properties
+    New-xDSCResource -Name $ResourceName -Path $ResourcePath -ClassVersion 1.0 -Force -Property $(
+    New-xDscResourceProperty -Name LogName -Type String -Attribute Key -Description "Name of the event log"
+    New-xDscResourceProperty -Name MaximumSizeInBytes -Type Sint64 -Attribute Write -Description "sizethat the event log file is allowed to be When the file reaches this maximum size it is considered full"
+    New-xDscResourceProperty -Name IsEnabled -Type Boolean -Attribute Write -Description "Enable or Disable the specified event log"
+    New-xDscResourceProperty -Name LogMode -Type String -Attribute Write -ValidateSet "AutoBackup","Circular","Retain"
+    New-xDscResourceProperty -Name SecurityDescriptor -Type String -Attribute Write
+    New-xDSCResourceProperty -Name LogPath -Type String -Attribute Write -Description "Path the log file should be stored in"
+    )
+
+#If needed, use Update-xDSCResource if you add/remove any parameters from the resource
+#Update-xDscResource -Name $ResourceName -Property $ProductKey,$Activate -ClassVersion 1.0 -Force
+Update-xDscResource -Name $ResourceName -ClassVersion 1.0 -Force -Property $(
+    New-xDscResourceProperty -Name LogName -Type String -Attribute Key -Description "Name of the event log"
+    New-xDscResourceProperty -Name MaximumSizeInBytes -Type Sint64 -Attribute Write -Description "sizethat the event log file is allowed to be When the file reaches this maximum size it is considered full"
+    New-xDscResourceProperty -Name IsEnabled -Type Boolean -Attribute Write -Description "Enable or Disable the specified event log"
+    New-xDscResourceProperty -Name LogMode -Type String -Attribute Write -ValidateSet "AutoBackup","Circular","Retain"
+    New-xDscResourceProperty -Name SecurityDescriptor -Type String -Attribute Write
+    New-xDSCResourceProperty -Name LogPath -Type String -Attribute Write -Description "Path the log file should be stored in"
+    )
+
+#Test the schema after updating or creating
+Test-xDscSchema -Path ($ResourcePath + 'DSCResources\' + $ResourceName + ("\$ResourceName" + '.schema.mof'))
