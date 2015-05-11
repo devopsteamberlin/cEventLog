e43f740a070aaaa8412039d8a06f48a92d295697
Jacob Benson <jacob.benson@gmail.com>
2015-05-11 15:59:45 -0500

    Uploading Tests

---
+++ Tests/cEventLog.Tests.ps1
@@ -0,0 +1,21 @@
+﻿#$Module = ".\DSCResources\cEventLog\cEventLog.psm1"
+# Make sure the module is not loaded already
+if (Get-Module cEventLog) {
+    Remove-Module cEventLog -Force -ErrorAction SilentlyContinue
+}
+
+$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+Import-Module "$Here\..\cEventLog" -Force -ErrorAction Stop
+
+    Describe "cEventLog"{
+
+        Context Test-TargetResource{
+            It 'Returns True if Log Exists'{
+                {Test-TargetResource -LogName "Application"} | Should Be $True
+            }
+            It 'Returns False if Log Does Not Exist'{
+                {Test-TargetResource -LogName "MyApp"} | Should be $False
+            }
+
+        }
+    }
